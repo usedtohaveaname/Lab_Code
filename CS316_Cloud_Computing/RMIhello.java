9d6a88e0cde50668f5f251c90701b2adbcc26cce
usedtohaveaname <rangerx369@gmail.com>
2019-03-10 16:29:21 +0800

    delete unused files

--- CS316_Cloud_Computing/RMIhello.java
+++
@@ -1,5 +0,0 @@
-public class RMIhello {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
--- CS316_Cloud_Computing/ServerAction.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-public interface ServerAction extends Remote{
-    public boolean login() throws RemoteException;
-    
-}
